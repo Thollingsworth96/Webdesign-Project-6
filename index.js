60bd98b537d7e2b19d3a2372a58964f5c9a017a1
Thollingsworth96 <37086910+Thollingsworth96@users.noreply.github.com>
2018-04-17 23:15:01 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+// set up text to print, each item in array is new line
+var aText = new Array(
+"Accept what is, let go of what was, and have faith in what will be.", 
+"Everyday is a new beginning. Take a deep breath & start again."
+);
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+ 
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+ 
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
+typewriter();
+
+
+// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "Click Me!";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  alert("Hope you enjoyed this page!");
+});
